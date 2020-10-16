48e7a4159777a843b30936ce321c6dab365bd340
Cassandra Zuria <czuria1@gmail.com>
2020-10-15 21:10:52 -0700

    Add GitHub configuration UI

--- Pipeliner/Common/ServiceTypeEnum.swift
+++ Pipeliner/Common/ServiceTypeEnum.swift
@@ -8,4 +8,23 @@
 import Foundation
 enum ServiceType: String, CaseIterable {
     case GITLAB = "GITLAB"
+    case GITHUB = "GITHUB"
+    
+    func urlPlaceholder() -> String {
+        switch self {
+        case .GITLAB:
+            return "http://gitlab.com"
+        case .GITHUB:
+            return "http://api.github.com/repos"
+        }
+    }
+    
+    func tokenDescriptionLink() -> URL {
+        switch self {
+        case .GITLAB:
+            return URL(string: "https://docs.gitlab.com/ee/user/profile/personal_access_tokens.html#creating-a-personal-access-token")!
+        case .GITHUB:
+            return URL(string: "https://docs.github.com/en/free-pro-team@latest/github/authenticating-to-github/creating-a-personal-access-token")!
+        }
+    }
 }
