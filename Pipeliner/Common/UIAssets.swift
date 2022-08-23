79da908746c5e28a57ea66afb6715fd896dd10d5
Jan Kučera <kucera.jan@hotmail.cz>
2022-08-23 16:40:37 +0200

    Define colors in enum

--- Pipeliner/Common/UIAssets.swift
+++ Pipeliner/Common/UIAssets.swift
@@ -18,3 +18,18 @@ enum Images {
         static let bitbucket = Image("bitbucket_svg_icon")
     }
 }
+
+enum Colors {
+    static let white4 = Color("white-4")
+    static let white10 = Color("white-10")
+    static let white60 = Color("white-60")
+    static let purple = Color("purple")
+    static let lightpurple = Color("lightpurple")
+    static let teal = Color("teal")
+    static let lightteal = Color("lightteal")
+    static let blue = Color("blue")
+    static let darkblue = Color("darkblue")
+    static let blueshadow = Color("blueshadow")
+    static let warning = Color("warning")
+    static let error = Color("error")
+}
