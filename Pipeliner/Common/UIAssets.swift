d842d51c070b992c41d29553e7ce14d5d76d3d3d
Jan Kučera <kucera.jan@hotmail.cz>
2022-08-23 16:20:28 +0200

    Add new logo images

---
+++ Pipeliner/Common/UIAssets.swift
@@ -0,0 +1,20 @@
+//
+//  Images.swift
+//  Pipeliner
+//
+//  Created by Jan Kučera on 23.08.2022.
+//
+
+import SwiftUI
+
+enum Images {
+    enum DXLogos {
+        static let green = Image("dx-logo-green")
+        static let white = Image("dx-logo-white")
+    }
+    enum Services {
+        static let github = Image("github_svg_icon")
+        static let gitlab = Image("gitlab_svg_icon")
+        static let bitbucket = Image("bitbucket_svg_icon")
+    }
+}
