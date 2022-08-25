a150dbf096ff5a5a07821295024a6ea2479f0221
Jan Kučera <kucera.jan@hotmail.cz>
2022-08-25 14:13:51 +0200

    First part of refactoring

--- Pipeliner/Views/PipelineDetailView.swift
+++ Pipeliner/Views/PipelineDetailView.swift
@@ -8,48 +8,116 @@
 import SwiftUI
 
 struct PipelineDetailView: View {
+
     public let index: Int
     public let pipeline: PipelineResult
-    @Environment(\.colorScheme) var colorScheme
+
     var body: some View {
-        HStack(content: {
-            Image( nsImage: pipeline.serviceType.serviceIcon())
-            VStack(alignment: .leading, content: {
-                HStack(content: {
-                    Text(pipeline.repositoryName.uppercased()).lineLimit(1)
-                })
-                Text(pipeline.ref).foregroundColor(Color("white-60")).lineLimit(1)
-            })
+
+        HStack {
+            pipeline.serviceType.servicePickerIcon()
+                .padding(.trailing, 8)
+            VStack(alignment: .leading) {
+                Text(pipeline.repositoryName.uppercased())
+                    .lineLimit(1)
+                Text(pipeline.ref)
+                    .foregroundColor(Colors.white60)
+                    .lineLimit(1)
+            }
             Spacer()
-            VStack(alignment: .leading, content: {
-                if pipeline.status.requiresTitle() {
-                    HStack(alignment: .center, content: {
-                        Text(pipeline.status.readableTitle()).foregroundColor(Color("white-60"))
-                        pipeline.status.icon()
-                    })
-                } else {
+            if pipeline.status.requiresTitle() {
+                HStack(alignment: .center) {
+                    Text(pipeline.status.readableTitle())
+                        .foregroundColor(Colors.white60)
                     pipeline.status.icon()
                 }
-            })
-            VStack(alignment: .leading, content: {
-                Text(pipeline.duration)
-                Text("\(pipeline.age) ago").foregroundColor(Color("white-60"))
-            }).frame(width: 80, height: 40)
-            Link(destination: URL(string: pipeline.url)!) {
-                Image(systemName: "link").font(.title2).foregroundColor(.blue)
-            }.padding(.horizontal).onHover { inside in
-                if inside {
-                    NSCursor.pointingHand.push()
-                } else {
-                    NSCursor.pop()
-                }
+            } else {
+                pipeline.status.icon()
             }
-        }).foregroundColor(Color.white).padding(.horizontal).padding([.vertical], self.index % 2 == 0 ? 8 : 0) .background(self.index % 2 == 0 ? Color("white-4") : Color("purple"))
+            VStack(alignment: .leading) {
+                Text(pipeline.duration)
+                Text("\(pipeline.age) ago")
+                    .foregroundColor(Colors.white60)
+            }
+            .frame(width: 80, height: 40)
+            Link(destination: URL(string: pipeline.url)!) {
+                Image(systemName: "link")
+                    .font(.title2)
+                    .foregroundColor(.blue)
+            }
+            .padding(.horizontal)
+            .onHover { $0 ? NSCursor.pointingHand.push() : NSCursor.pop() }
+        }
+        .foregroundColor(Color.white)
+        .padding(.horizontal)
+        .padding(.vertical, index % 2 == 0 ? 8 : 0)
+        .background(index % 2 == 0 ? Colors.white4 : Colors.purple)
+
     }
 }
 
 struct PipelineDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        PipelineDetailView(index: 0, pipeline: PipelineResult(id: 1, ref: "renovate/minor-version-update", status: PipelineStatus.FAILED, duration: "54 min", age: "4 min", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github))
+        PipelineDetailView(
+            index: 0,
+            pipeline: PipelineResult(
+                id: 1,
+                ref: "renovate/minor-version-update",
+                status: PipelineStatus.FAILED,
+                duration: "54 min",
+                age: "4 min",
+                url: "url",
+                repositoryName: "Cool Project",
+                serviceType: ServiceType.github
+            )
+        )
+        .background(Colors.purple)
+
+        PipelineDetailView(
+            index: 0,
+            pipeline: PipelineResult(
+                id: 1,
+                ref: "renovate/minor-version-update",
+                status: PipelineStatus.MANUAL,
+                duration: "54 min",
+                age: "4 min",
+                url: "url",
+                repositoryName: "Cool Project",
+                serviceType: ServiceType.github
+            )
+        )
+        .background(Colors.darkblue)
+        .background(Colors.white4)
+
+        PipelineDetailView(
+            index: 0,
+            pipeline: PipelineResult(
+                id: 1,
+                ref: "renovate/minor-version-update",
+                status: PipelineStatus.PREPARING,
+                duration: "54 min",
+                age: "4 min",
+                url: "url",
+                repositoryName: "Cool Project",
+                serviceType: ServiceType.github
+            )
+        )
+        .background(Colors.purple)
+
+        PipelineDetailView(
+            index: 0,
+            pipeline: PipelineResult(
+                id: 1,
+                ref: "renovate/minor-version-update",
+                status: PipelineStatus.SCHEDULED,
+                duration: "54 min",
+                age: "4 min",
+                url: "url",
+                repositoryName: "Cool Project",
+                serviceType: ServiceType.github
+            )
+        )
+        .background(Colors.darkblue)
+        .background(Colors.white4)
     }
 }
