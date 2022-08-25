5743654fe22574d042453d86aa424dc72d892968
Jan Kučera <kucera.jan@hotmail.cz>
2022-08-25 18:47:26 +0200

    Adjust code structure + make mock data for previews

--- PipelinerWidget/PipelinerWidget.swift
+++ PipelinerWidget/PipelinerWidget.swift
@@ -13,38 +13,60 @@ struct Provider: IntentTimelineProvider {
     let service: PipelinerService = PipelinerService()
     
     func placeholder(in context: Context) -> SimpleEntry {
-        return SimpleEntry(date: Date(), pipelines: [PipelineResult(id: 1, ref: "test", status: PipelineStatus.FAILED, duration: "54 min", age: "4 min", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github)], error: false)
+        SimpleEntry.provideOneEntry()
     }
     
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry: SimpleEntry
-        // use mock data
-        if(context.isPreview) {
-            switch context.family {
-            case .systemSmall:
-                entry = SimpleEntry(date: Date(), pipelines: [PipelineResult(id: 1, ref: "test", status: PipelineStatus.FAILED, duration: "4 min", age: "54 min", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github)], error: false)
-            case .systemMedium:
-                entry = SimpleEntry(date: Date(), pipelines: [PipelineResult(id: 1, ref: "fix:test", status: PipelineStatus.SUCCESS, duration: "4 min", age: "54 min", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github), PipelineResult(id: 2, ref: "feat: make it cooler", status: PipelineStatus.SUCCESS, duration: "6 min", age: "4 hours", url: "url", repositoryName: "Second Project", serviceType: ServiceType.github), PipelineResult(id: 3, ref: "chore: clean up", status: PipelineStatus.FAILED, duration: "9 min", age: "8 hours", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github)
-                ], error: false)
-            case .systemLarge:
-                entry = SimpleEntry(date: Date(), pipelines: [PipelineResult(id: 1, ref: "fix:test", status: PipelineStatus.SUCCESS, duration: "4 min", age: "54 min", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github), PipelineResult(id: 2, ref: "feat: make it cooler", status: PipelineStatus.SUCCESS, duration: "6 min", age: "4 hours", url: "url", repositoryName: "Second Project", serviceType: ServiceType.github), PipelineResult(id: 3, ref: "chore: clean up", status: PipelineStatus.FAILED, duration: "9 min", age: "8 hours", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github), PipelineResult(id: 4, ref: "fix:test", status: PipelineStatus.SUCCESS, duration: "4 min", age: "9 hours", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github), PipelineResult(id: 5, ref: "feat: make it cooler", status: PipelineStatus.SUCCESS, duration: "6 min", age: "10 hours", url: "url", repositoryName: "Second Project", serviceType: ServiceType.github), PipelineResult(id: 6, ref: "chore: clean up", status: PipelineStatus.FAILED, duration: "9 min", age: "13 hours", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github)
-                ], error: false)
-            default:
-                entry = SimpleEntry(date: Date(), pipelines: [PipelineResult(id: 1, ref: "fix:test", status: PipelineStatus.SUCCESS, duration: "4 min", age: "54 min", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github), PipelineResult(id: 2, ref: "feat: make it cooler", status: PipelineStatus.SUCCESS, duration: "6 min", age: "4 hours", url: "url", repositoryName: "Second Project", serviceType: ServiceType.github), PipelineResult(id: 3, ref: "chore: clean up", status: PipelineStatus.FAILED, duration: "9 min", age: "8 hours", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github)
-                ], error: false)
-            }
-            // use real data
-        } else {
-            entry = SimpleEntry(date: Date(), pipelines: try! service.getPipelines(pipelineCount: getPipelineCount(context: context)), error: false)
-        }
-        completion(entry)
+    func getSnapshot(
+        for configuration: ConfigurationIntent,
+        in context: Context,
+        completion: @escaping (SimpleEntry) -> ()
+    ) {
+//        let entry: SimpleEntry
+//        // use mock data
+//        if (context.isPreview) {
+//            switch context.family {
+//            case .systemSmall:
+//                entry = SimpleEntry(date: Date(), pipelines: [PipelineResult(id: 1, ref: "test", status: PipelineStatus.FAILED, duration: "4 min", age: "54 min", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github)], error: false)
+//            case .systemMedium:
+//                entry = SimpleEntry(date: Date(), pipelines: [PipelineResult(id: 1, ref: "fix:test", status: PipelineStatus.SUCCESS, duration: "4 min", age: "54 min", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github), PipelineResult(id: 2, ref: "feat: make it cooler", status: PipelineStatus.SUCCESS, duration: "6 min", age: "4 hours", url: "url", repositoryName: "Second Project", serviceType: ServiceType.github), PipelineResult(id: 3, ref: "chore: clean up", status: PipelineStatus.FAILED, duration: "9 min", age: "8 hours", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github)
+//                ], error: false)
+//            case .systemLarge:
+//                entry = SimpleEntry(date: Date(), pipelines: [PipelineResult(id: 1, ref: "fix:test", status: PipelineStatus.SUCCESS, duration: "4 min", age: "54 min", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github), PipelineResult(id: 2, ref: "feat: make it cooler", status: PipelineStatus.SUCCESS, duration: "6 min", age: "4 hours", url: "url", repositoryName: "Second Project", serviceType: ServiceType.github), PipelineResult(id: 3, ref: "chore: clean up", status: PipelineStatus.FAILED, duration: "9 min", age: "8 hours", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github), PipelineResult(id: 4, ref: "fix:test", status: PipelineStatus.SUCCESS, duration: "4 min", age: "9 hours", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github), PipelineResult(id: 5, ref: "feat: make it cooler", status: PipelineStatus.SUCCESS, duration: "6 min", age: "10 hours", url: "url", repositoryName: "Second Project", serviceType: ServiceType.github), PipelineResult(id: 6, ref: "chore: clean up", status: PipelineStatus.FAILED, duration: "9 min", age: "13 hours", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github)
+//                ], error: false)
+//            default:
+//                entry = SimpleEntry(date: Date(), pipelines: [PipelineResult(id: 1, ref: "fix:test", status: PipelineStatus.SUCCESS, duration: "4 min", age: "54 min", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github), PipelineResult(id: 2, ref: "feat: make it cooler", status: PipelineStatus.SUCCESS, duration: "6 min", age: "4 hours", url: "url", repositoryName: "Second Project", serviceType: ServiceType.github), PipelineResult(id: 3, ref: "chore: clean up", status: PipelineStatus.FAILED, duration: "9 min", age: "8 hours", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.github)
+//                ], error: false)
+//            }
+//            // use real data
+//        } else {
+//            entry = SimpleEntry(date: Date(), pipelines: try! service.getPipelines(pipelineCount: getPipelineCount(context: context)), error: false)
+//        }
+//        completion(entry)
+
+        let entrySnapshot = SimpleEntry(
+            date: Date(),
+            error: false,
+            pipelines: try! service.getPipelines(pipelineCount: getPipelineCount(context: context))
+        )
+        completion(entrySnapshot)
     }
     
-    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+    func getTimeline(
+        for configuration: ConfigurationIntent,
+        in context: Context,
+        completion: @escaping (Timeline<Entry>) -> ()
+    ) {
         let now = Date()
         let nextUpdate = Calendar.current.date(byAdding: .minute, value: 1, to: now)!
         let data = try! service.getPipelines(pipelineCount: getPipelineCount(context: context))
-        let timeline = Timeline(entries: [SimpleEntry(date: Date(), pipelines: data, error: false)], policy: .after(nextUpdate))
+        let timeline = Timeline(
+            entries: [SimpleEntry(
+                date: Date(),
+                error: false,
+                pipelines: data
+            )],
+            policy: .after(nextUpdate)
+        )
         completion(timeline)
     }
     
@@ -56,15 +78,13 @@ struct Provider: IntentTimelineProvider {
             return 3
         case .systemLarge:
             return 6
+        @unknown default:
+            return 1
         }
     }
 }
 
-struct SimpleEntry: TimelineEntry {
-    let date: Date
-    let pipelines: [PipelineResult]
-    let error: Bool
-}
+
 
 struct PipelinerWidgetEntryView : View {
 
@@ -117,13 +137,13 @@ struct PipelinerWidget: Widget {
 
 struct PipelinerWidget_Previews: PreviewProvider {
     static var previews: some View {
-        PipelinerWidgetEntryView(entry: SimpleEntry(date: Date(), pipelines: [PipelineResult(id: 1, ref: "test", status: PipelineStatus.FAILED, duration: "54 min", age: "4 min", url: "url", repositoryName: "Cool project", serviceType: ServiceType.github)], error: false))
+        PipelinerWidgetEntryView(entry: SimpleEntry.provideOneEntry())
             .previewContext(WidgetPreviewContext(family: .systemSmall))
 
-        PipelinerWidgetEntryView(entry: SimpleEntry(date: Date(), pipelines: [PipelineResult(id: 1, ref: "test", status: PipelineStatus.FAILED, duration: "54 min", age: "4 min", url: "url", repositoryName: "Cool project", serviceType: ServiceType.github)], error: false))
+        PipelinerWidgetEntryView(entry: SimpleEntry.provideMultipleEntries())
             .previewContext(WidgetPreviewContext(family: .systemMedium))
 
-        PipelinerWidgetEntryView(entry: SimpleEntry(date: Date(), pipelines: [PipelineResult(id: 1, ref: "test", status: PipelineStatus.FAILED, duration: "54 min", age: "4 min", url: "url", repositoryName: "Cool project", serviceType: ServiceType.github)], error: false))
+        PipelinerWidgetEntryView(entry: SimpleEntry.provideMultipleEntries())
             .previewContext(WidgetPreviewContext(family: .systemLarge))
     }
 }
