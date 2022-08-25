41e1b8db6362408d843fe44af3f14a72e0e27035
Jan Kučera <kucera.jan@hotmail.cz>
2022-08-25 19:13:22 +0200

    Changed to mock data structure

--- PipelinerWidget/SimpleEntry.swift
+++ PipelinerWidget/SimpleEntry.swift
@@ -12,57 +12,96 @@ struct SimpleEntry: TimelineEntry {
     let error: Bool
     let pipelines: [PipelineResult]
 
-    static func provideOneEntry() -> SimpleEntry {
-        let pipelineResult = PipelineResult(
-            id: 1,
-            ref: "test",
-            status: PipelineStatus.FAILED,
-            duration: "4 min",
-            age: "54 min",
-            url: "url",
-            repositoryName: "Cool Project",
-            serviceType: ServiceType.github
-        )
-        return SimpleEntry(date: Date(), error: false, pipelines: [pipelineResult])
-    }
-    static func provideMultipleEntries() -> SimpleEntry {
-        let pipelineResult1 = PipelineResult(
+    struct MockData {
+        static let pipelineResult1 = PipelineResult(
             id: 1,
             ref: "test",
             status: PipelineStatus.SUCCESS,
             duration: "4 min",
-            age: "54 min",
+            age: "54min",
             url: "url",
             repositoryName: "Cool Project",
             serviceType: ServiceType.github
         )
-        let pipelineResult2 = PipelineResult(
+        static let pipelineResult2 = PipelineResult(
             id: 2,
             ref: "test",
             status: PipelineStatus.FAILED,
             duration: "14 min",
-            age: "54 min",
+            age: "54min",
             url: "url",
             repositoryName: "Failing Project",
             serviceType: ServiceType.gitlab
         )
-        let pipelineResult3 = PipelineResult(
+        static let pipelineResult3 = PipelineResult(
             id: 3,
             ref: "test",
             status: PipelineStatus.MANUAL,
             duration: "114 min",
-            age: "54 min",
+            age: "54min",
             url: "url",
             repositoryName: "Another Cool Project",
             serviceType: ServiceType.github
         )
-        return SimpleEntry(
-            date: Date(),
-            error: false,
-            pipelines: [
-                pipelineResult1, pipelineResult2,
-                pipelineResult3
-            ]
+        static let pipelineResult4 = PipelineResult(
+            id: 4,
+            ref: "test",
+            status: PipelineStatus.SUCCESS,
+            duration: "4 min",
+            age: "54min",
+            url: "url",
+            repositoryName: "Cool Project",
+            serviceType: ServiceType.github
         )
+        static let pipelineResult5 = PipelineResult(
+            id: 5,
+            ref: "test",
+            status: PipelineStatus.FAILED,
+            duration: "14 min",
+            age: "54min",
+            url: "url",
+            repositoryName: "Failing Project",
+            serviceType: ServiceType.gitlab
+        )
+        static let pipelineResult6 = PipelineResult(
+            id: 6,
+            ref: "test",
+            status: PipelineStatus.MANUAL,
+            duration: "114 min",
+            age: "54min",
+            url: "url",
+            repositoryName: "Another Cool Project",
+            serviceType: ServiceType.github
+        )
+
+        static func smallWidget() -> SimpleEntry {
+            SimpleEntry(
+                date: Date(),
+                error: false,
+                pipelines: [pipelineResult1]
+            )
+        }
+        static func mediumWidget() -> SimpleEntry {
+            SimpleEntry(
+                date: Date(),
+                error: false,
+                pipelines: [
+                    pipelineResult1,
+                    pipelineResult2,
+                    pipelineResult3
+                ]
+            )
+        }
+        static func largeWidget() -> SimpleEntry {
+            SimpleEntry(
+                date: Date(),
+                error: false,
+                pipelines: [
+                    pipelineResult1, pipelineResult2,
+                    pipelineResult3, pipelineResult4,
+                    pipelineResult5, pipelineResult6
+                ]
+            )
+        }
     }
 }
