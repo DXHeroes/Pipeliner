87557c9ce84fc9b46ff188c139c7ccf9060b38af
Jan Kučera <kucera.jan@hotmail.cz>
2022-08-25 15:41:08 +0200

    Refactor widget code
    - refactoring part 1
    - adding Assets as a target to widgets

--- PipelinerWidget/PipelineRowView.swift
+++ PipelinerWidget/PipelineRowView.swift
@@ -7,60 +7,138 @@
 
 import SwiftUI
 import WidgetKit
+
 struct PipelineRowView: View {
+
     public let pipeline: PipelineResult
     public let size: WidgetFamily
     public let isOdd: Bool
-    //@Environment(\.colorScheme) var colorScheme
     
     var body: some View {
         switch size {
         case .systemSmall:
-            VStack(alignment: .center, content: {
-                HStack(content: {
-                    Image(nsImage: pipeline.serviceType.serviceIcon())
-                    Text(pipeline.repositoryName.uppercased()).lineLimit(1)
-                }).padding(.top, 5)
-                Text(pipeline.ref).foregroundColor(Color("white-60")).lineLimit(1)
+            VStack(alignment: .center) {
+                HStack {
+                    pipeline.serviceType.servicePickerIcon()
+                    Text(pipeline.repositoryName.uppercased())
+                        .lineLimit(1)
+                }
+                .padding(.top, 5)
+                Text(pipeline.ref)
+                    .foregroundColor(Colors.white60)
+                    .lineLimit(1)
                 Spacer()
-                if(pipeline.status == PipelineStatus.FAILED) {
-                    Image(systemName: "xmark").font(.system(size: 60)).foregroundColor(Color("error"))
-                } else if(pipeline.status == PipelineStatus.SUCCESS) {
-                    Image(systemName: "checkmark").font(.system(size: 60)).foregroundColor(Color("ligtteal"))
+                if (pipeline.status == PipelineStatus.FAILED) {
+                    Image(systemName: "xmark")
+                        .font(.system(size: 60))
+                        .foregroundColor(Colors.error)
+                } else if (pipeline.status == PipelineStatus.SUCCESS) {
+                    Image(systemName: "checkmark")
+                        .font(.system(size: 60))
+                        .foregroundColor(Colors.lightteal)
                 } else {
-                    Image(systemName: "arrow.triangle.2.circlepath").font(.system(size: 60)).foregroundColor(Color("warning"))
+                    Image(systemName: "arrow.triangle.2.circlepath")
+                        .font(.system(size: 60))
+                        .foregroundColor(Colors.warning)
                 }
                 Spacer()
                 Text(pipeline.duration)
-                Text("\(pipeline.age) ago").foregroundColor(Color("white-60"))
-            }).environment(\.colorScheme, .dark)
+                Text("\(pipeline.age) ago")
+                    .foregroundColor(Colors.white60)
+            }
+            .environment(\.colorScheme, .dark)
         default:
-            HStack(content: {
-                Image( nsImage: pipeline.serviceType.serviceIcon())
-                VStack(alignment: .leading, content: {
-                    Text(pipeline.repositoryName.uppercased()).lineLimit(1)
-                    Text(pipeline.ref).foregroundColor(Color("white-60")).lineLimit(1)
-                })
-                Spacer()
-                if(pipeline.status == PipelineStatus.FAILED) {
-                    Image(systemName: "xmark").font(.title).foregroundColor(Color("error"))
-                } else if(pipeline.status == PipelineStatus.SUCCESS) {
-                    Image(systemName: "checkmark").font(.title).foregroundColor(Color("lightteal"))
-                } else {
-                    Image(systemName: "arrow.triangle.2.circlepath").font(.title).foregroundColor(Color("warning"))
+            HStack {
+                pipeline.serviceType.servicePickerIcon()
+                VStack(alignment: .leading) {
+                    Text(pipeline.repositoryName.uppercased())
+                        .lineLimit(1)
+                    Text(pipeline.ref)
+                        .foregroundColor(Colors.white60)
+                        .lineLimit(1)
                 }
-                VStack(alignment: .leading, content: {
+                Spacer()
+                if (pipeline.status == PipelineStatus.FAILED) {
+                    Image(systemName: "xmark")
+                        .font(.title)
+                        .foregroundColor(Colors.error)
+                } else if (pipeline.status == PipelineStatus.SUCCESS) {
+                    Image(systemName: "checkmark")
+                        .font(.title)
+                        .foregroundColor(Colors.lightteal)
+                } else {
+                    Image(systemName: "arrow.triangle.2.circlepath")
+                        .font(.title)
+                        .foregroundColor(Colors.warning)
+                }
+                VStack(alignment: .leading) {
                     Text(pipeline.duration)
-                    Text("\(pipeline.age) ago").foregroundColor(Color("white-60"))
-                }).frame(width: 80, height: 40)
-            }).foregroundColor(Color.white).padding(.horizontal).padding([.vertical], isOdd ? 0 : 8).background(isOdd ? Color("WidgetBackground") : Color("white-4")).environment(\.colorScheme, .dark)
+                    Text("\(pipeline.age) ago")
+                        .foregroundColor(Colors.white60)
+                }
+                .frame(width: 80, height: 40)
+            }
+            .foregroundColor(Color.white)
+            .padding(.horizontal)
+            .padding(.vertical, isOdd ? 0 : 8)
+            .background(isOdd ? Colors.widgetBackground : Colors.white4)
+            .environment(\.colorScheme, .dark)
         }
     }
 }
 
+// MARK: - Preview
 
 struct PipelineRowView_Previews: PreviewProvider {
+
     static var previews: some View {
-        PipelineRowView(pipeline: PipelineResult(id: 1, ref: "test", status: PipelineStatus.FAILED, duration: "54 min", age: "4 min", url: "url", repositoryName: "Cool Project", serviceType: ServiceType.gitlab), size: .systemMedium, isOdd: false)
+        PipelineRowView(
+            pipeline: PipelineResult(
+                id: 1,
+                ref: "test",
+                status: PipelineStatus.FAILED,
+                duration: "54 min",
+                age: "4 min",
+                url: "url",
+                repositoryName: "Cool Project",
+                serviceType: ServiceType.gitlab
+            ),
+            size: .systemMedium,
+            isOdd: false
+        )
+        .previewContext(WidgetPreviewContext(family: .systemSmall))
+
+        PipelineRowView(
+            pipeline: PipelineResult(
+                id: 1,
+                ref: "test",
+                status: PipelineStatus.FAILED,
+                duration: "54 min",
+                age: "4 min",
+                url: "url",
+                repositoryName: "Cool Project",
+                serviceType: ServiceType.gitlab
+            ),
+            size: .systemMedium,
+            isOdd: false
+        )
+        .previewContext(WidgetPreviewContext(family: .systemMedium))
+
+        PipelineRowView(
+            pipeline: PipelineResult(
+                id: 1,
+                ref: "test",
+                status: PipelineStatus.FAILED,
+                duration: "54 min",
+                age: "4 min",
+                url: "url",
+                repositoryName: "Cool Project",
+                serviceType: ServiceType.gitlab
+            ),
+            size: .systemMedium,
+            isOdd: false
+        )
+        .previewContext(WidgetPreviewContext(family: .systemLarge))
     }
+    
 }
