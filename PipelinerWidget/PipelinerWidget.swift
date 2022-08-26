483c0e1a970ef34e259b5353eb72c6fc49b8429a
Jan Kučera <kucera.jan@hotmail.cz>
2022-08-26 12:45:44 +0200

    Update widget flow (configuration, provider etc.)

--- PipelinerWidget/PipelinerWidget.swift
+++ PipelinerWidget/PipelinerWidget.swift
@@ -5,63 +5,67 @@
 //  Created by dx hero on 03.09.2020.
 //
 
-import WidgetKit
 import SwiftUI
-import Intents
+import WidgetKit
 
-struct Provider: IntentTimelineProvider {
-    let service: PipelinerService = PipelinerService()
-    
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry.MockData.smallWidget()
-    }
-    
-    func getSnapshot(
-        for configuration: ConfigurationIntent,
-        in context: Context,
-        completion: @escaping (SimpleEntry) -> ()
-    ) {
-        let entry: SimpleEntry
-        if (context.isPreview) { // use mock data
-            switch context.family {
-            case .systemSmall:
-                entry = SimpleEntry.MockData.smallWidget()
-            case .systemMedium:
-                entry = SimpleEntry.MockData.mediumWidget()
-            case .systemLarge:
-                entry = SimpleEntry.MockData.largeWidget()
-            default:
-                entry = SimpleEntry.MockData.smallWidget()
-            }
-        } else { // use real data
-            entry = SimpleEntry(
-                date: Date(),
-                error: false,
-                pipelines: try! service.getPipelines(pipelineCount: getPipelineCount(context: context))
-            )
+@main
+struct PipelinerWidget: Widget {
+    let kind: String = "PipelinerWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(
+            kind: kind,
+            provider: Provider()
+        ) { entry in
+            PipelinerWidgetEntryView(entry: entry)
         }
-        completion(entry)
+        .configurationDisplayName("Pipeliner Widget")
+        .description("See your pipelines!")
     }
-    
-    func getTimeline(
-        for configuration: ConfigurationIntent,
+}
+
+public struct Provider: TimelineProvider {
+    public typealias Entry = SimpleEntry
+    let service: PipelinerService = PipelinerService()
+
+    public func placeholder(in context: Context) -> SimpleEntry {
+        switch context.family {
+        case .systemSmall:
+            return SimpleEntry.MockData.smallWidget()
+        case .systemMedium:
+            return SimpleEntry.MockData.mediumWidget()
+        case .systemLarge:
+            return SimpleEntry.MockData.largeWidget()
+        @unknown default:
+            return SimpleEntry.MockData.smallWidget()
+        }
+    }
+
+    public func getSnapshot(
         in context: Context,
-        completion: @escaping (Timeline<Entry>) -> ()
+        completion: @escaping (SimpleEntry) -> Void
     ) {
-        let now = Date()
-        let nextUpdate = Calendar.current.date(byAdding: .minute, value: 1, to: now)!
-        let data = try! service.getPipelines(pipelineCount: getPipelineCount(context: context))
+        // Snapshot of current state
+        // Get here real data somehow
+        completion(SimpleEntry.MockData.largeWidget())
+    }
+
+    public func getTimeline(
+        in context: Context,
+        completion: @escaping (Timeline<SimpleEntry>) -> Void
+    ) {
+        let nextUpdate = Calendar.current.date(byAdding: .hour, value: 3, to: Date())!
+        // Get here real data somehow
         let timeline = Timeline(
-            entries: [SimpleEntry(
-                date: Date(),
-                error: false,
-                pipelines: data
-            )],
+            entries: [SimpleEntry.MockData.largeWidget()],
             policy: .after(nextUpdate)
         )
         completion(timeline)
     }
-    
+
+}
+
+extension Provider {
     func getPipelineCount(context: Context) -> Int {
         switch context.family {
         case .systemSmall:
@@ -76,62 +80,4 @@ struct Provider: IntentTimelineProvider {
     }
 }
 
-struct PipelinerWidgetEntryView: View {
-    var entry: Provider.Entry
-    @Environment(\.widgetFamily) var family
-
-    var body: some View {
-        ZStack {
-            Colors.widgetBackground
-                .edgesIgnoringSafeArea(.all)
-            if (entry.pipelines.isEmpty) {
-                Text("No data")
-                    .foregroundColor(Colors.white60)
-            } else {
-                VStack(spacing: 0) {
-                    ForEach(
-                        Array(entry.pipelines.enumerated()),
-                        id: \.element.id
-                    ) { index, pipeline in
-                        PipelineRowView(
-                            pipeline: pipeline,
-                            isOdd: index % 2 != 0
-                        )
-                    }
-                }
-            }
-        }
-    }
-}
-
-@main
-struct PipelinerWidget: Widget {
-    let kind: String = "PipelinerWidget"
-    
-    var body: some WidgetConfiguration {
-        IntentConfiguration(
-            kind: kind,
-            intent: ConfigurationIntent.self,
-            provider: Provider()
-        ) { entry in
-            PipelinerWidgetEntryView(entry: entry)
-        }
-        .configurationDisplayName("Pipeliner Widget")
-        .description("See your pipelines!")
-    }
-}
-
-// MARK: - Previews
-
-struct PipelinerWidget_Previews: PreviewProvider {
-    static var previews: some View {
-        PipelinerWidgetEntryView(entry: SimpleEntry.MockData.smallWidget())
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
-
-        PipelinerWidgetEntryView(entry: SimpleEntry.MockData.mediumWidget())
-            .previewContext(WidgetPreviewContext(family: .systemMedium))
-
-        PipelinerWidgetEntryView(entry: SimpleEntry.MockData.largeWidget())
-            .previewContext(WidgetPreviewContext(family: .systemLarge))
-    }
-}
+// let data = try! service.getPipelines(pipelineCount: getPipelineCount(context: context))
